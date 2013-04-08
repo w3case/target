6bbf653147c534b2d5531c8f7d3d486123635f44
Luiz Picolo <luizpicolo@luizpicolo.com.br>
2013-04-08 11:02:22 -0400

    Adicionado comentários

--- assets/admin/js/main.js
+++ assets/admin/js/main.js
@@ -1,4 +1,5 @@
-$(window).scroll(function (event) {
+// Função para manipulação do menu flutuante
+$(window).scroll(function(event) {
     var scroll = $(this).scrollTop();
 
     if (scroll >= 240)
@@ -11,10 +12,73 @@ $(window).scroll(function (event) {
     }
 });
 
-if($('.tooltip-link').length) {
-	$('.tooltip-link').tooltip();
+// Função para manipulação do ToolTip
+if ($('.tooltip-link').length) {
+    $('.tooltip-link').tooltip();
 }
 
-if($('.textarea').length) {
-	$('.textarea').wysihtml5();
+if ($('.textarea').length) {
+    $('.textarea').wysihtml5();
 }
+
+// Função Jquery para envio de formulário
+// Dependencia: jqueryForm
+//$(document).ready(function() {
+$("#form").submit(function() {
+    /**
+     *  retorna o valor do atributo Data remote do formulário enviado
+     *  @return String
+     */
+    var dataRemote = $(this).attr("data-remote");
+
+    // Verifa se o formulário será enviado remotamente
+    if (dataRemote == "true")
+    {
+        /**
+         * Retorna o caminho para a ação do formulário
+         * @return String
+         */
+        var action = $(this).attr("action");
+
+        /**
+         * Retorna o ID da tag que será o foco
+         * das mensagens de retorno
+         * 
+         * @return String
+         */
+        var focusResponse = $(this).attr("focus-response");
+
+        /**
+         * Retorna o Methodo utilizado para o envio do 
+         * formulário
+         * 
+         * @return String
+         */
+        var method = $(this).attr("method");
+
+        // Funções do jqueryForm
+        $(this).ajaxStart(function() {
+            $(focusResponse).html("Aguarde...").show();
+        });
+
+        // Variáveis
+        var options = {
+            target: focusResponse,
+            url: action,
+            type: method,
+            success: function(resposta) {
+                $(focusResponse).html(resposta).show();
+            },
+            error: function(xhr, textStatus, errorThrown) {
+                $(focusResponse).html(xhr).show();
+            }
+        }
+
+        $(this).ajaxSubmit(options);
+
+        // Retorna-se false para que o formulário não seja
+        // enviado pelo methodo tradicional
+        return false;
+    }
+})
+//})
